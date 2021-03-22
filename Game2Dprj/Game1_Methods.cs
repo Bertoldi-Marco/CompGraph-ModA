f745182f25a1bd1d4f026f8b548647584d774be7
VignaRiccardo <supericcardo98@hotmail.it>
2021-03-22 20:04:34 +0100

    inserita la classe per il gioco HittingGame.cs
    Signed-off-by: VignaRiccardo <supericcardo98@hotmail.it>

---
+++ Game2Dprj/Game1_Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Game2Dprj
+{
+    public static class Game1_Methods
+    {
+        public static void Camera_movement(Rectangle viewSource, Point mouseDiff, int xScreenDim, int yScreenDim, Texture2D background)
+        {
+            if (viewSource.X > 0 && mouseDiff.X < 0)
+            {
+                viewSource.X += mouseDiff.X;
+                if (viewSource.X < 0)
+                    viewSource.X = 0;
+            }
+            //Saturation on the right
+            if (viewSource.X < background.Width - xScreenDim && mouseDiff.X > 0)
+            {
+                viewSource.X += mouseDiff.X;
+                if (viewSource.X > background.Width - xScreenDim)
+                    viewSource.X = background.Width - xScreenDim;
+            }
+            //Saturation on the top
+            if (viewSource.Y > 0 && mouseDiff.Y < 0)
+            {
+                viewSource.Y += mouseDiff.Y;
+                if (viewSource.Y < 0)
+                    viewSource.Y = 0;
+            }
+            //Saturation on the bottom
+            if (viewSource.Y < background.Height - yScreenDim && mouseDiff.Y > 0)
+            {
+                viewSource.Y += mouseDiff.Y;
+                if (viewSource.Y > background.Height - yScreenDim)
+                    viewSource.Y = background.Height - yScreenDim;
+            }
+        }
+    }
+}
---
+++ Game2Dprj/HittingGame.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game2Dprj
+{
+    public class HittingGame
+    {
+        //-------------------------------Internal variables
+        //Background
+        private Texture2D background;
+        private Point backgroundStart;
+        private Rectangle viewSource;
+        private Rectangle viewDest;
+        //Cursor
+        private Rectangle cursorRect;
+        private Texture2D cursor;
+        //Target
+        private Texture2D target;
+        private Color targetColor;
+        private Point targetPosition;
+        private Rectangle targetRect;
+        Point targetDim;
+        //Mouse
+        private MouseState newMouse;
+        private MouseState oldMouse;
+        private Point mouseDiff;
+        //screen
+        private int xScreenDim;
+        private int yScreenDim;
+        private Point middleScreen;
+
+        Random rand;
+
+        public HittingGame(Point backgroundStart, Rectangle viewSource, Rectangle viewDest, Rectangle cursorRect, Color targetColor, int xScreenDim, int yScreenDim, Point middleScreen, Texture2D background, Texture2D cursor, Texture2D target)
+        {
+            rand = new Random();
+            targetDim = new Point(10, 10);
+            this.backgroundStart = backgroundStart;
+            this.viewSource = viewSource;
+            this.viewDest = viewDest;
+            this.cursorRect = cursorRect;
+            this.targetColor = targetColor;
+            targetPosition = new Point(rand.Next(xScreenDim / 8, xScreenDim - xScreenDim / 8), targetPosition.Y = rand.Next(yScreenDim / 8, yScreenDim - yScreenDim / 8));
+            targetRect = new Rectangle(targetPosition, targetDim);
+            oldMouse = Mouse.GetState();
+            this.xScreenDim = xScreenDim;
+            this.yScreenDim = yScreenDim;
+            this.middleScreen = middleScreen;
+            this.background = background;
+            this.cursor = cursor;
+            this.target = target;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            newMouse = Mouse.GetState();
+            Mouse.SetPosition(middleScreen.X, middleScreen.Y);
+
+            mouseDiff.X = newMouse.X - middleScreen.X;
+            mouseDiff.Y = newMouse.Y - middleScreen.Y;
+
+            Game1_Methods.Camera_movement(viewSource, mouseDiff, xScreenDim, yScreenDim, background);
+
+            if (newMouse.LeftButton == ButtonState.Pressed && oldMouse.LeftButton == ButtonState.Released)
+            {
+                if (targetRect.Contains(middleScreen))
+                {
+                    targetPosition.X = rand.Next(xScreenDim / 8, xScreenDim - xScreenDim / 8);
+                    targetPosition.Y = rand.Next(yScreenDim / 8, yScreenDim - yScreenDim / 8);
+                }
+            }
+            else
+            {
+                targetPosition.X -= mouseDiff.X;
+                targetPosition.Y -= mouseDiff.Y;
+            }
+
+            targetRect = new Rectangle(targetPosition, targetDim);
+            oldMouse = newMouse;               //this is necessary to store the previous value of left button
+        }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(target, targetRect, Color.White);
+            _spriteBatch.Draw(cursor, cursorRect, Color.White);
+        }
+    }
+
+}
